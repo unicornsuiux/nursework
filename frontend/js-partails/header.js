c87febe48fda7381101ca959e1fbfc9afab4201f
danish334 <danish@cynosuredesigns.co.uk>
2023-04-05 00:19:26 +0500

    new page added

--- frontend/js-partails/header.js
+++ frontend/js-partails/header.js
@@ -1,33 +1,102 @@
-{/* <header class="header" id="header">
-    <nav class="navbar navbar-light">
-        <div class="container">
-            <a href="./index.html" class="navbar-brand">
-                <h3 class="mb-0">nurse<span>work</span></h3>
-            </a>
-            <div class="header_links">
-                <li class="link_item">
-                    <a href="./find-jobs.html">
-                        <img class="icon-w" src="./frontend/img/find-magnifier.svg" alt="">
-                        Find Job
-                    </a>
-                </li>
-                <li class="link_item">
-                    <a href="#">
-                        <img class="icon-w" src="./frontend/img/post-it.svg" alt="">
-                        Post Job
-                    </a>
-                </li>
-                <li class="link_item">
-                    <a href="#">
-                        <img class="icon-w" src="./frontend/img/notification.svg" alt="">
-                        Notification
-                    </a>
-                </li>
-            </div>
-            <div class="d-flex gap-2 align-items-center">
-                <li><a href="#" class="">Sign In</a></li>
-                <li><a href="#" class="theme_btn text-white">Sign Up</a></li>
-            </div>
-        </div>
-    </nav>
-</header> */}
+document.write('<header class="header" id="header">');
+document.write('    <nav class="navbar navbar-light fixed-top">');
+document.write('        <div class="container">');
+document.write('            <a href="./index.html" class="navbar-brand">');
+document.write('                <img src="./frontend/img/logo.jpg" class="obj_fit_contain" alt="">');
+document.write('            </a>');
+document.write('            <div class="header_links">');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        Facilities');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        Professionals');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        Resources');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        About');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        Corporate Careers');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        Contact');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('                <li class="link_item">');
+document.write('                    <a class="nav-link" href="#">');
+document.write('                        Resources');
+document.write('                    </a>');
+document.write('                </li>');
+document.write('            </div>');
+document.write('            <div class="d-flex gap-2 align-items-center">');
+document.write('                <li><a href="#" class="theme_btn text-white">Sign Up</a></li>');
+document.write('                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"');
+document.write('                    data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">');
+document.write('                    <span class="navbar-toggler-icon"></span>');
+document.write('                </button>');
+document.write('            </div>');
+document.write('            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"');
+document.write('                aria-labelledby="offcanvasDarkNavbarLabel">');
+document.write('                <div class="offcanvas-header">');
+document.write('                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>');
+document.write('                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"');
+document.write('                        aria-label="Close"></button>');
+document.write('                </div>');
+document.write('                <div class="offcanvas-body">');
+document.write('                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link active" aria-current="page" href="#">');
+document.write('                                Facilities');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link" href="#">');
+document.write('                                Professionals');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link" href="#">');
+document.write('                                Resources');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link" href="#">');
+document.write('                                About');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link" href="#">');
+document.write('                                Corporate Careers');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link" href="#">');
+document.write('                                Contact');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                        <li class="link_item">');
+document.write('                            <a class="nav-link" href="#">');
+document.write('                                Resources');
+document.write('                            </a>');
+document.write('                        </li>');
+document.write('                    </ul>');
+document.write('                </div>');
+document.write('            </div>');
+document.write('        </div>');
+document.write('    </nav>');
+document.write('</header>');
+
+

1492bbc70a87a9891917a773bb5376d424e9fd3d
danish334 <danish@cynosuredesigns.co.uk>
2023-04-11 02:29:15 +0500

    add new screeen

--- frontend/js/main-custom.js
+++ frontend/js/main-custom.js
@@ -304,6 +304,21 @@ $('.visa-no').click(function(){
 
 
 });
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
+});
   
   
   
